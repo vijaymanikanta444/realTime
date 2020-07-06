3a1b6bbb51e2316e42bfd440927301299710a9af
vijaymanikanta444 <vijayakp444@gmail.com>
2020-07-06 21:47:39 +0530

    reset password

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import decode from 'jwt-decode';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import rootReducer from './rootReducer';
+import { userLoggedIn } from './actions/auth';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+if (localStorage.bookwormJWT) {
+  const payload = decode(localStorage.bookwormJWT);
+  const user = {
+    token: localStorage.bookwormJWT,
+    email: payload.email,
+    confirmed: payload.confirmed,
+  };
+  store.dispatch(userLoggedIn(user));
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <Route component={App} />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
